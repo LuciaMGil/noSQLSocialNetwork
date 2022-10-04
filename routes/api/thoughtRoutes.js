90d33aec39578dd5ea253c753dcd05a53cdd8cba
Lucia Gil <luciagil5952@gmail.com>
2022-10-04 13:00:53 -0400

    fixed typo

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,7 +7,7 @@ router.route('/thoughts').get(getThoughts).post(newThought)
 // GET, DELTE AND UPDATE THOUGHTS BY ID
 router.route('/thoughts/:id').get(getThoughtById).delete(deleteThought).put(updateThought)
 
-// POST AND DELETE A REACTION
+// POST
 router.route('/thoughts/:thoughtId/reactions').post(newReaction)
 
 // DELETE A REACTION
