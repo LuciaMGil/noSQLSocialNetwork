362ed64493e8373629d576496c3ebc946b1a9431
Lucia Gil <luciagil5952@gmail.com>
2022-10-04 10:59:35 -0400

    Added post,put,delete, and get for users and added friends

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,67 @@
 const { User, Thought } = require ('../models');
 
+
+const usersController = {
+    createUser({ body }, res){
+        User.create(body)
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+    },
+
+    getUsers(req, res) {
+        User.find({}).populate({path: 'friends',select: '-__v'}).populate({path: 'thoughts',select: '-__v'}).select('-__v')
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+    updateUser({params, body} , res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(data =>{
+            if (!data) {
+                res.status(404).json({message: 'Invalid ID'});
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => res.json(err));
+    },
+
+    deleteUser({params}, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then(data => res.json({message:`Successfully deleted user! ${data}`}))
+          .catch(err => res.json(err));
+      },
+
+      getUserID ({params}, res) {
+        User.findOne ({_id: params.id }).populate({path: 'thoughts', select: '-__v'}).select('-__v')
+        .then(data => res.json(data))
+        .catch(err =>{
+            console.log(err);
+            res.sendStatus(400);
+        } );
+    },
+      addFriends ({params}, res) {
+        User.findOneAndUpdate({_id: params.userId}, {$push: { friends: params.friendId}}, {new: true})
+        .then(data => {
+        console.log(data)
+        res.json(data)
+        })
+        .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+        });
+      },
+    
+      deleteFriend({ params }, res) {
+        User.findOneAndUpdate({_id:params.userId}, {$pull: { friends: params.friendId}}, {new: true})
+          .then(data => res.json(data))
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+          });
+      }
+};
+
+module.exports = usersController;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
-const {getAllUsers} = require('../../controllers/users');
+const {getUsers, getUserID, createUser, updateUser, deleteUser, addFriends, deleteFriend} = require('../../controllers/users');
 
-// User route
-router.route('/users').get(getAllUsers)
+//GET AND CREATE USERS
+router.route('/users').get(getUsers).post(createUser)
+
+//GET, DELETE, AND UPDATE USERS BY ID
+
+router.route('/users/:id').get(getUserID).delete(deleteUser).put(updateUser)
+
+// POST AND DELETE FRIENDS BY ID
+router.route('/users/:userId/friends/:friendId').post(addFriends).delete(deleteFriend)
 
 
 module.exports = router; 
